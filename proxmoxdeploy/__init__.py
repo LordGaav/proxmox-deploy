1c6ddf384c24bb3db49fd8650601beea9eb404ef
Nick Douma <n.douma@nekoconeko.nl>
2015-12-22 22:14:49 +0100

    Add project files

---
+++ proxmoxdeploy/__init__.py
@@ -0,0 +1,16 @@
+# proxmox-deploy is cli-based deployment tool for Proxmox
+#
+# Copyright (c) 2015 Nick Douma <n.douma@nekoconeko.nl>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see http://www.gnu.org/licenses/.
